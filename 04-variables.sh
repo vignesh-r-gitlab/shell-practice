a260349b3211ae7eed2ef2b4887a66a89a3700ea
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-01 21:23:05 +0530

    shell practice

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=$1
+name2=$2
+
+echo "$name1: how are you $name2?"
+echo "$name2: Hi, $name1 i am doing good, how are you?"
+echo "$name1: i am fine, what your learning $name2?"
+echo "$name2: i am learning shell"
