8880bbf12b6fd3384648a31f6ae665acfc910334
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-01 21:18:48 +0530

    shell practice

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=suresh
+name2=ramesh
+
+echo "$name1: how are you $name2?"
+echo "$name2: Hi, $name1 i am doing good, how are you?"
+echo "$name1: i am fine, what your learning $name2?"
+echo "$name2: i am learning shell"
