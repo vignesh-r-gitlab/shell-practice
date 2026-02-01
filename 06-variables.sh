6d2a3350bcd58b44c3f265b57bc1fe78e013e526
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-01 22:19:32 +0530

    shell practice

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,8 +3,8 @@
 starttime=$(date +%s)
 echo "script start time: $starttime"
 
-endtime=$(data +%s)
-endtime "script end time:$endtime"
+endtime=$(date +%s)
+echo "script end time:$endtime"
 
 totaltime=$($endtime-$starttime)
 
