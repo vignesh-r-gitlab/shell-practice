cf355535bff212240850c18d59c93e57a9663fc3
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-01 19:45:30 +0530

    shell practice

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh: how are you remesh?"
+echo "ramesh: Hi, suresh i am doing good, how are you?"
+echo "suresh: i am fine, what your learning ramesh?"
+echo "ramesh: i am learning shell"
