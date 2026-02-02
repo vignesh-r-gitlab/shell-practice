9b8a7f3e8c9cb7485b5834131014584f7e9305c1
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-02 23:02:37 +0530

    shell practice

---
+++ 12-logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG-DIR="/var/log/shell-script"
+LOG-FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "please execute script with root user"
+    exit 1
+fi
+
+mkdir -p $LOG-DIR
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then 
+    echo "$2".....failure
+else
+    echo "$2".....success
+
+}
+
+dnf install nginx -y
+VALIDATE $? "nginx installation is" 
