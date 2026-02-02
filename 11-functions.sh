410e437b06264f996ce653db7b43178ff540b4e1
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-02 18:16:07 +0530

    shell practice

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please run the script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+    echo "$2.....failure "
+    exit 1
+else
+    echo "$2.....success"
+
+}
+
+dnf install nginx -y
+VALIDATE $? "installation is"
+
+dnf install mysql -y
+VALIDATE $? "installation is"
+
+dnf install nodejs -y
+VALIDATE $? "installation is"
