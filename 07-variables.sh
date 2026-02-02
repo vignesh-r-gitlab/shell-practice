dcfb21afc20a30b43cc3c6e41173647210eda859
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-02 16:38:20 +0530

    shell practice

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "$@"
-echo "$#"
-echo "$0"
-echo "$PWD"
-echo "$USER"
-echo "$HOME"
-echo "$$"
+echo "all arguments passed to script:$@"
+echo "number of variables passed to script:$#"
+echo "script name:$0"
+echo "present working directory:$PWD"
+echo "who is running:$USER"
+echo "home directory of current user:$HOME"
+echo "PID of the script:$$"
 sleep 10 &
-echo "$!"
-echo "$*"
-echo "$?"
+echo "PID of the recently background running process:$!"
+echo "all aarguments passed to the script:$*"
+echo "exit status of previous command:$?"
