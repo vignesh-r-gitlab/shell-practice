fb3a9794344b296a5e2eea2e32fb52487b62acac
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-02 23:12:36 +0530

    shell practice

--- 12-logs.sh
+++ 12-logs.sh
@@ -15,9 +15,9 @@ mkdir -p $LOG_DIR
 VALIDATE(){
 
 if [ $1 -ne 0 ]; then 
-    echo "$2".....failure   &>> $LOG_FILE
+    echo "$2".....failure   | tee -a $LOG_FILE
 else
-    echo "$2".....success   &>> $LOG_FILE
+    echo "$2".....success   | tee -a $LOG_FILE
 fi
 }
 
