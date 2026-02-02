2e27706114eda58d5d0d84ef93a14882fee524d7
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-02 17:33:27 +0530

    shell practice

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "please run the script with root user"
     exit 1
+
 else
     echo "installing nginx"
     dnf install nginx -y
@@ -15,5 +16,6 @@ if [ $? -ne 0 ]; then
     echo "installation......failure"
 
 else
+
     echo "installation......success"
 fi
