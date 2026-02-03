4afff2f55d964c0da0212aa9828c05c76c78ef9f
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-03 20:58:30 +0530

    shell practice

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
-LOG_DIR="/var/log/shell-practice"
-LOG_FILE="/var/log/shell-practice/$0.log"
-
-if [ $USERID -ne 0 ]; then
-    echo "please run the script with root user"
-    exit 1
-fi
-
-mkdir -p $LOG_DIR
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-    echo "$2......failure"  &>> $LOG_FILE
-    exit 1
-else
-    echo "$2.....success"   &>> $LOG_FILE
-fi
-}
-
-
-for package in $@
-do 
-    dnf install $package -y &>> $LOG_FILE
-    VALIDATE $? "$package installation is"
+for i in {0,1,2,3,....10}
+do
+    echo "$i"
 done
