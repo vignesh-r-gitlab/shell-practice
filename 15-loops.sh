75e836d3b4a1f8721dd53ec754bd30b42fbadcb9
vignesh-r <vigneshrajendran115@gmail.com>
2026-02-03 21:10:15 +0530

    shell practice

--- 15-loops.sh
+++ 15-loops.sh
@@ -23,6 +23,10 @@ fi
 
 for package in $@
 do 
-    dnf install $package -y | tee -a $LOG_FILE
+    dnf list installed $package     | tee -a $LOG_FILE
+    if [ $? -ne 0 ]; then
+    dnf install $package -y         | tee -a $LOG_FILE
     VALIDATE $? "$package installation is"
+else
+    echo "package already installed"
 done
